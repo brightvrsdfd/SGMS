637eb8ef91086c7cc562bb838be6aba5e5d517db
brightvrsdfd <vrsdfd@163.com>
2023-12-12 20:31:22 +0800

    zhq1212

---
+++ TeachingSystem.cpp
@@ -0,0 +1,143 @@
+#include "TeachingSystem.h"
+#include <vector>
+
+using namespace std;
+
+TeachingSystem::TeachingSystem()
+{
+    fileSys.CreateDir("User");
+    fileSys.CreateDir("Course");
+    // TeachingSystem::CreateUser("root","root", 1);
+}
+
+string TeachingSystem::getLoginStatus(string username, string password)
+{
+    string role = fileSys.getFileCont("User/" + username + "/" + password);
+
+    if (role == "")
+        return "ERROR";
+
+    return role;
+}
+
+string TeachingSystem::PrintCourse()
+{
+    vector<string> list = fileSys.getListVector("Course");
+    if (list.size() == 0)
+    {
+        return "There is no course.\n";
+    }
+
+    string res = "There are these courses:\n";
+    for (int i = 0; i < list.size(); i++)
+    {
+        res = res + "    " + list[i] + "\n";
+    }
+    return res;
+}
+
+string TeachingSystem::ListUser()
+{
+    vector<string> list = fileSys.getListVector("User");
+    if (list.size() == 0)
+    {
+        return "There is no user in the system.\n";
+    }
+
+    string res = "There are these users in this system:\n";
+    for (int i = 0; i < list.size(); i++)
+    {
+        res = res + "    " + list[i] + "\n";
+    }
+    return res;
+}
+
+void TeachingSystem::CreateUser(string username, string password, string role)
+{
+    string path = "User/" + username;
+    fileSys.CreateDir(path);
+
+    path = path + "/" + password;
+    fileSys.CreateFile(path);
+
+    fileSys.WriteFile(path, role);
+
+    // fileSys.WriteFile(path, string(1, char(role)));
+}
+
+void TeachingSystem::DeleteUser(string username)
+{
+    string path = "User/" + username;
+    fileSys.Delete(path);
+}
+
+void TeachingSystem::CreateCourse(std::string teacherName, std::string courseName)
+{
+    string path = "Course/" + teacherName + "/" + courseName;
+    fileSys.CreateDir(path);
+}
+
+void TeachingSystem::DeleteCourse(std::string teacherName, std::string courseName)
+{
+    string path = "Course/" + teacherName + "/" + courseName;
+    fileSys.Delete(path);
+}
+
+void TeachingSystem::CreateAssignment(std::string teacherName, std::string courseName, std::string assignment, std::string content)
+{
+    string path = "Course/" + teacherName + "/" + courseName + "/" + assignment;
+    fileSys.CreateDir(path);
+    fileSys.CreateFile(path + "/Description");
+    fileSys.WriteFile(path + "/Description", content);
+}
+
+void TeachingSystem::DeleteAssignment(std::string teacherName, std::string courseName, std::string assignment, std::string content)
+{
+    string path = "Course/" + teacherName + "/" + courseName + "/" + assignment;
+    fileSys.Delete(path);
+}
+
+void TeachingSystem::SubmitAssignment(std::string teacherName, std::string studentName, std::string courseName, std::string assignment, std::string content)
+{
+    string path = "Course/" + teacherName + "/" + courseName + "/" + assignment + "/" + studentName;
+    fileSys.CreateDir(path);
+    fileSys.CreateFile(path + "/submission");
+    fileSys.CreateFile(path + "/mark");
+    fileSys.WriteFile(path + "/submission", content);
+}
+
+void TeachingSystem::DeleteSubmission(std::string teacherName, std::string studentName, std::string courseName, std::string assignment, std::string content)
+{
+    string path = "Course/" + teacherName + "/" + courseName + "/" + assignment + "/" + studentName;
+    fileSys.Delete(path);
+}
+
+void TeachingSystem::MarkSubmission(std::string teacherName, std::string studentName, std::string courseName, std::string assignment, std::string mark)
+{
+    string path = "Course/" + teacherName + "/" + courseName + "/" + assignment + "/" + studentName + "/mark";
+    fileSys.WriteFile(path, mark);
+}
+
+std::string TeachingSystem::getCourseInfo(std::string teacherName, std::string courseName)
+{
+    std::string path = "Course/" + teacherName + "/" + courseName;
+    std::string courseList = fileSys.getListTree(path);
+    return courseList;
+}
+
+std::string TeachingSystem::getAssignmentInfo(std::string teacherName, std::string courseName, std::string assignment)
+{
+    string path = "Course/" + teacherName + "/" + courseName + "/" + assignment + "/Description";
+    std::string assignmentInfo = fileSys.getFileCont(path);
+    return assignmentInfo;
+}
+
+std::string TeachingSystem::getSubmissionInfo(std::string teacherName, std::string courseName, std::string assignment, std::string studentName)
+{
+    string path = "Course/" + teacherName + "/" + courseName + "/" + assignment + "/" + studentName + "/submission";
+    std::string subInfo = fileSys.getFileCont(path);
+    return subInfo;
+}
+
+void Backup();
+void Recovery();
--- TeachingSystem.h
+++ TeachingSystem.h
@@ -1,40 +1,6 @@
 #pragma once
-
 #include "FileSystem.h"
 
-/*
-File Structure:
-(root)
-|
-|---User
-|   |-username1
-|   |   └-password(role is in this file)
-|   |-username2
-|   |   └-password
-|   ...
-|
-└---Course
-    |-course_name1
-    |   |-assignment1
-    |   |   |-Description
-    |   |   └-Homework
-    |   |   |-title1
-    |   |   |   |-Score  
-    |   |   |   └-Content
-    |   |   |-title2
-    |   |   |   |-Score
-    |   |   |   └-Content
-    |   |       ...
-    |   |
-    |   |-assignment2
-    |   |   └-...
-    |   ...
-    |
-    |-course_name2
-    |   └-...
-    ...
-
-*/
 class TeachingSystem
 {
 public:
@@ -48,18 +14,19 @@ public:
     void CreateUser(std::string username, std::string password, std::string role);
     void DeleteUser(std::string username);
 
-    void CreateCourse(std::string courseName);
-    void DeleteCourse(std::string courseName);
+    void CreateCourse(std::string teacherName, std::string courseName);
+    void DeleteCourse(std::string teacherName, std::string courseName);
 
-    void CreateAssignment(std::string courseName, std::string assignment, std::string content);
-    std::string getHomworkTitles(std::string courseName, std::string assignment);
-    std::string getHomeworkContent(std::string courseName, std::string assignment, std::string title);
-    void MarkHomework(std::string courseName, std::string assignment, std::string title, std::string score);
+    void CreateAssignment(std::string teacherName, std::string courseName, std::string assignment, std::string content);
+    void DeleteAssignment(std::string teacherName, std::string courseName, std::string assignment, std::string content);
 
-    std::string getAssignmentTitles(std::string courseName); 
-    std::string getAssignmentContent(std::string courseName, std::string assignment);
-    void SubmitAssignment(std::string courseName, std::string assignment, std::string title, std::string content);
-    std::string getScoreInCourse(std::string courseName, std::string assignment, std::string title);
+    void SubmitAssignment(std::string teacherName, std::string studentName, std::string courseName, std::string assignment, std::string content);
+    void DeleteSubmission(std::string teacherName, std::string studentName, std::string courseName, std::string assignment, std::string content);
+    void MarkSubmission(std::string teacherName, std::string studentName, std::string courseName, std::string assignment, std::string mark);
+
+    std::string getCourseInfo(std::string teacherName, std::string courseName);
+    std::string getAssignmentInfo(std::string teacherName, std::string courseName, std::string assignment);
+    std::string getSubmissionInfo(std::string teacherName, std::string courseName, std::string assignment, std::string studentName);
 
     void Backup();
     void Recovery();
